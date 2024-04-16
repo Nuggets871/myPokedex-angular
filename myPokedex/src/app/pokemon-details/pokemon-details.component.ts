4c64abba08fb8bdfe18844b038c1ba844ebb98e6
Christopher <christopherbondier@gmail.com>
2024-04-16 17:59:14 +0200

    cpn pkm detail

---
+++ myPokedex/src/app/pokemon-details/pokemon-details.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Pokemon } from '../pokemon';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-pokemon-details',
+  templateUrl: './pokemon-details.component.html',
+  styleUrl: './pokemon-details.component.scss'
+})
+export class PokemonDetailsComponent implements OnInit {
+  @Input() pokemon?: Pokemon;
+
+  constructor(
+    private route : ActivatedRoute,
+  ) { }
+
+  ngOnInit(): void {
+    this.getPokemon();
+  }
+
+  getPokemon(): void {
+   const id = Number(this.route.snapshot.paramMap.get("id"))
+   
+  }
+
+}
