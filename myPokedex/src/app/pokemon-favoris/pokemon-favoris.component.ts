4c5240fb2bae63155fbcf3df7c525f330b0ea9b5
Christopher <christopherbondier@gmail.com>
2024-04-16 17:59:05 +0200

    component pkm fav

---
+++ myPokedex/src/app/pokemon-favoris/pokemon-favoris.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-favoris',
+  templateUrl: './pokemon-favoris.component.html',
+  styleUrl: './pokemon-favoris.component.scss'
+})
+export class PokemonFavorisComponent {
+
+}
